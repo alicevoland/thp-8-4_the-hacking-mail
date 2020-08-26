f74d9f5aec9a2f774afa8dea63917655cf483615
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-26 19:00:47 +0200

    AJAX destroy a bit better

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,18 +1,29 @@
 class EmailsController < ApplicationController
   def show
+    @page_title = 'Lire le mail'
     @email = Email.find(params[:id])
     respond_to do |format|
-      format.html { redirect_to email_path(@email) }
+      format.html {}
       format.js {}
     end
   end
 
   def index
+    @page_title = 'Boite de réception'
     @emails = Email.all.sort_by(&:created_at).reverse
   end
 
   def create
     @email = Email.create_fake
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js {}
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js {}
