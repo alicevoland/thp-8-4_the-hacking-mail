277b2eeeb8d06ad4b5299cd8377cc989666fd7eb
Mathieu Voland <mathieu.voland@gmail.com>
2020-08-27 18:56:52 +0200

    mark as read when showed

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,6 +2,7 @@ class EmailsController < ApplicationController
   def show
     @page_title = 'Lire le mail'
     @email = Email.find(params[:id])
+    @email.update(read: true)
     respond_to do |format|
       format.html {}
       format.js {}
